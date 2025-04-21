bd5a51367f4a591f38e050435d6fb03042f75014
yusuf <yusuf210409@gmail.com>
2025-04-21 09:53:59 +0700

    finishing ThreadRepository Domain

---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,12 @@
+const ThreadRepository = require('../ThreadRepository')
+
+describe('ThreadRepository domain', () => {
+    it('should throw error when access abstract behavior', async () => {
+        // Arrange
+        const threadRepository = new ThreadRepository()
+
+        // Action and Assert
+        await expect(threadRepository.addThread({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+        await expect(threadRepository.getThread({})).rejects.toThrow('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    })
+})
