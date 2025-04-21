76b7873afb105157aefcf0a9b787267cc2a21cb6
yusuf <yusuf210409@gmail.com>
2025-04-21 08:59:29 +0700

    finishing add thread domain entities

---
+++ src/Domains/threads/entities/_test/AddThread.test.js
@@ -0,0 +1,40 @@
+const AddThread = require('../AddThread')
+
+describe('AddThread entities', () => {
+    it('should throw error when payload not contain needed property', () => {
+        // Arrange
+        const payload = {
+            title: 'thread title'
+        }
+
+        // Action and Assert
+        expect(() => new AddThread(payload)).toThrow('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY')
+    })
+
+    it('should throw error when payload not meet data type specification', () => {
+        // Arrange
+        const payload = {
+            title: 'thread title',
+            body: false
+        }
+
+        // Action and Assert
+        expect(() => new AddThread(payload)).toThrow('ADD_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')
+    })
+
+    it('should create addThread object correctly', () => {
+        // Arrange
+        const payload = {
+            title: 'thread title',
+            body: 'thread body'
+        }
+
+        // Action
+        const addThread = new AddThread(payload)
+
+        // Assert
+        expect(addThread.title).toEqual(payload.title)
+        expect(addThread.body).toEqual(payload.body)
+        expect(addThread).toBeInstanceOf(AddThread)
+    })
+})
--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -2,14 +2,14 @@
 const { Pool } = require('pg');
 const config = require('../../../Commons/config')
 
-// const testConfig = {
-//   host: process.env.PGHOST_TEST,
-//   port: process.env.PGPORT_TEST,
-//   user: process.env.PGUSER_TEST,
-//   password: process.env.PGPASSWORD_TEST,
-//   database: process.env.PGDATABASE_TEST,
-// };
+const testConfig = {
+  host: process.env.PGHOST_TEST,
+  port: process.env.PGPORT_TEST,
+  user: process.env.PGUSER_TEST,
+  password: process.env.PGPASSWORD_TEST,
+  database: process.env.PGDATABASE_TEST,
+};
 
-const pool = new Pool(config.database);
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool(config.database);
 
 module.exports = pool;
